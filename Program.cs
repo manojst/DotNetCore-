9e280e2219f1d7e923c0515252625eb6edffa5e8
Manojkumar Gnanasekaran(UST <172389@ust-global.com>
2021-10-11 21:44:11 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSCodeDotNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Welcome to DotNET Core app");
+        }
+    }
+}
